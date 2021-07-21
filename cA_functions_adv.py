8adb8b46e53d83d587a40c213112594d2ae6026d
srac1777 <shashank.racherla@gmail.com>
2021-07-21 10:24:47 -0400

    cA more practice

---
+++ cA_functions_adv.py
@@ -0,0 +1,69 @@
+# Write your first_three_multiples function here
+def first_three_multiples(num):
+  print(num*1)
+  print(num*2)
+  print(num*3)
+  return num*3
+
+
+# Uncomment these function calls to test your first_three_multiples function:
+first_three_multiples(10)
+# should print 10, 20, 30, and return 30
+first_three_multiples(0)
+# should print 0, 0, 0, and return 0
+
+# Write your introduction function here:
+
+# Write your tip function here:
+
+
+def tip(total, percentage):
+  return total*percentage/100
+
+
+# Uncomment these function calls to test your tip function:
+print(tip(10, 25))
+# should print 2.5
+print(tip(0, 100))
+# should print 0.0
+
+
+def introduction(f, l):
+  return l+", "+f+" "+l
+
+
+# Uncomment these function calls to test your introduction function:
+print(introduction("James", "Bond"))
+# should print Bond, James Bond
+print(introduction("Maya", "Angelou"))
+# should print Angelou, Maya Angelou
+
+
+# Write your dog_years function here:
+def dog_years(name, age):
+  return name+", you are "+str(age*7)+" years old in dog years"
+
+
+# Uncomment these function calls to test your dog_years function:
+print(dog_years("Lola", 16))
+# should print "Lola, you are 112 years old in dog years"
+print(dog_years("Baby", 0))
+# should print "Baby, you are 0 years old in dog years"
+
+# Write your lots_of_math function here:
+
+
+def lots_of_math(a, b, c, d):
+  e = a+b
+  f = c-d
+  print(e)
+  print(f)
+  print(e*f)
+  return (e*f) % a
+
+
+# Uncomment these function calls to test your lots_of_math function:
+print(lots_of_math(1, 2, 3, 4))
+# should print 3, -1, -3, 0
+print(lots_of_math(1, 1, 1, 1))
+# should print 2, 0, 0, 0
