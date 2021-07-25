d8252c6333ccef418a9822b715e2ac0889d56676
srac1777 <shashank.racherla@gmail.com>
2021-07-24 21:45:06 -0400

    dictionary update method

--- Codecademy/dictionaries.py
+++ Codecademy/dictionaries.py
@@ -1,2 +1,9 @@
 children = {"von Trapp": ["Johannes", "Rosmarie",
                           "Eleonore"], "Corleone": ["Sonny", "Fredo", "Michael"]}
+
+
+user_ids = {"teraCoder": 9018293, "proProgrammer": 119238}
+
+user_ids.update({ "theLooper": 138475, "stringQueen": 85739 })
+
+print(user_ids)
