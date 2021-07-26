0f07ae2a1fafa2548fb6cb6c3b8028f88f315b35
srac1777 <shashank.racherla@gmail.com>
2021-07-26 09:35:49 -0400

    json files

--- Codecademy/cA_files.py
+++ Codecademy/cA_files.py
@@ -1,2 +1,9 @@
-with open("test.txt", "w") as file_doc:
-  file_doc.write("this is a test!")
+# with open("test.txt", "w") as file_doc:
+#   file_doc.write("this is a test!")
+
+import json
+
+data_payload = {"k": 4}
+
+with open("data.json", "w") as data_json:
+  json.dump(data_payload, data_json)
