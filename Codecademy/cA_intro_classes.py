974d6f07601da79a54ccacf8a088ccd1a7850d75
srac1777 <shashank.racherla@gmail.com>
2021-07-28 09:39:10 -0400

    class variables 2

--- Codecademy/cA_intro_classes.py
+++ Codecademy/cA_intro_classes.py
@@ -27,3 +27,33 @@ print(codecademy.secure())
 
 print(wikipedia.secure())
 # prints "https://www.wikipedia.org"
+
+
+class Student:
+  def __init__(self, name, year):
+    self.name = name
+    self.year = year
+    self.grades = []
+
+  def add_grade(self, grade):
+    if type(grade) == Grade:
+      self.grades.append(grade)
+
+
+roger = Student("Roger van der Weyden", 10)
+sandro = Student("Sandro Botticelli", 12)
+pieter = Student("Pieter Bruegel the Elder", 8)
+
+
+class Grade:
+  minimum_passing = 65
+
+  def __init__(self, score):
+    self.score = score
+
+  def is_passing(self):
+    return self.score >= self.minimum_passing
+
+
+gr = Grade(100)
+pieter.add_grade(gr)
