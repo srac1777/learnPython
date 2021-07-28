81591ec91c6123ee98ffee2b1af4280c675e9e49
srac1777 <shashank.racherla@gmail.com>
2021-07-28 09:19:11 -0400

    class variables

--- Codecademy/cA_intro_classes.py
+++ Codecademy/cA_intro_classes.py
@@ -7,3 +7,23 @@ class Circle:
 
 
 teaching_table = Circle(36)
+
+
+class SearchEngineEntry:
+  secure_prefix = "https://"
+
+  def __init__(self, url):
+    self.url = url  # <--- here we are defining an instance variable and setting it
+
+  def secure(self):
+    return "{prefix}{site}".format(prefix=self.secure_prefix, site=self.url)
+
+
+codecademy = SearchEngineEntry("www.codecademy.com")
+wikipedia = SearchEngineEntry("www.wikipedia.org")
+
+print(codecademy.secure())
+# prints "https://www.codecademy.com"
+
+print(wikipedia.secure())
+# prints "https://www.wikipedia.org"
