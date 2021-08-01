41f86e89c0fcd8c58817be24963f8393ec9819cf
srac1777 <shashank.racherla@gmail.com>
2021-08-01 10:18:55 -0400

    str2

--- Codecademy/cA_strings_basic.py
+++ Codecademy/cA_strings_basic.py
@@ -20,3 +20,18 @@ print(unique_english_letters("mississippi"))
 # should print 4
 print(unique_english_letters("Apple"))
 # should print 4
+
+
+def count_char_x(word, x):
+  count = 0
+  for letter in word:
+    if letter == x:
+      count += 1
+  return count
+
+
+# Uncomment these function calls to test your tip function:
+print(count_char_x("mississippi", "s"))
+# should print 4
+print(count_char_x("mississippi", "m"))
+# should print 1
