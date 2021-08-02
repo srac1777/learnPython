c36561bf2cc211ee09441abf624c7fc2f4ba5af9
srac1777 <shashank.racherla@gmail.com>
2021-08-02 08:10:26 -0400

    str4

--- Codecademy/cA_strings_basic.py
+++ Codecademy/cA_strings_basic.py
@@ -48,3 +48,20 @@ print(count_multi_char_x("mississippi", "iss"))
 # should print 2
 print(count_multi_char_x("apple", "pp"))
 # should print 1
+
+
+# Write your substring_between_letters function here:
+
+def substring_between_letters(word, start, end):
+  s = word.find(start)
+  e = word.find(end)
+  if s != -1 and e != -1:
+    return word[s+1:e]
+  return word
+
+
+# Uncomment these function calls to test your function:
+print(substring_between_letters("apple", "p", "e"))
+# should print "pl"
+print(substring_between_letters("apple", "p", "c"))
+# should print "apple"
