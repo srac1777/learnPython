08c636e9a271f499d8e4149cccc3f132653d0bc9
srac1777 <shashank.racherla@gmail.com>
2021-08-02 08:36:47 -0400

    dict_basic1,2

---
+++ Codecademy/cA_dictionaries_basic.py
@@ -0,0 +1,26 @@
+# Write your sum_values function here:
+def sum_values(m):
+  vals = list(m.values())
+  return sum(vals)
+
+
+# Uncomment these function calls to test your sum_values function:
+print(sum_values({"milk": 5, "eggs": 2, "flour": 3}))
+# should print 10
+print(sum_values({10: 1, 100: 2, 1000: 3}))
+# should print 6
+
+# Write your sum_even_keys function here:
+def sum_even_keys(d):
+  total = 0
+  for key in d.keys():
+    if key % 2 == 0:
+      total += d[key]
+  return total
+
+
+# Uncomment these function calls to test your  function:
+print(sum_even_keys({1: 5, 2: 2, 3: 3}))
+# should print 2
+print(sum_even_keys({10: 1, 100: 2, 1000: 3}))
+# should print 6
