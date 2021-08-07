0ca890e8ee79b5f8cd6c5ca1ea486d0af630f2e3
srac1777 <shashank.racherla@gmail.com>
2021-08-07 09:02:34 -0400

    keyword argument unpacking

--- Codecademy-Intermediate/function-args.py
+++ Codecademy-Intermediate/function-args.py
@@ -55,3 +55,33 @@ assign_table(2, "Arwa", True)
 assign_and_print_order(2, "Steak", "Seabass", "Wine Bottle")
 
 print(tables)
+
+
+tables = {
+  1: {
+    'name': 'Chioma',
+    'vip_status': False,
+    'order': {
+      'drinks': 'Orange Juice, Apple Juice',
+      'food_items': 'Pancakes'
+    }
+  },
+  2: {},
+  3: {},
+  4: {},
+  5: {},
+  6: {},
+  7: {},
+}
+print(tables)
+
+
+# Write your code below: 
+def assign_food_items(**order_items):
+   print(order_items)
+   food = order_items.get("food")
+   drinks = order_items.get("drinks")
+   print(food)
+   print(drinks)
+# Example Call
+assign_food_items(food='Pancakes, Poached Egg', drinks='Water')
