4de94d525ffc4e7c85521bbdfb9da9d86011e65f
srac1777 <shashank.racherla@gmail.com>
2021-08-08 09:24:54 -0400

    namespaces

--- Codecademy-Intermediate/function-args.py
+++ Codecademy-Intermediate/function-args.py
@@ -10,17 +10,17 @@ tables = {
 print(tables)
 
 # Write your code below: 
-def assign_table(table_number, name, vip_status = False):
-  tables[table_number] = [name, vip_status]
+# def assign_table(table_number, name, vip_status = False):
+#   tables[table_number] = [name, vip_status]
 
-assign_table(6, "Yoni", False)
+# assign_table(6, "Yoni", False)
 
 
-assign_table(name="Martha", table_number=3, vip_status=True)
+# assign_table(name="Martha", table_number=3, vip_status=True)
 
-assign_table(4, "Karla")
+# assign_table(4, "Karla")
 
-print(tables)
+# print(tables)
 
 
 
