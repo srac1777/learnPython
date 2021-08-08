b8ad338d20acf2d6f127967873cbd86b204eb091
srac1777 <shashank.racherla@gmail.com>
2021-08-08 09:36:22 -0400

    namespaces2

--- Codecademy-Intermediate/namespaces.py
+++ Codecademy-Intermediate/namespaces.py
@@ -21,3 +21,39 @@ print(' \n -- Globals Namespace non-empty script -- \n')
 
 
 print(globals())
+
+
+
+global_variable = 'global'
+
+
+
+print(' -- Global Namespace with empty script -- \n')
+# Write Checkpoint 1 here:
+print(locals())
+print(globals())
+
+print(' \n -- Local Namespace with empty script -- \n')
+# Write Checkpoint 2 here:
+def divide(num1, num2):
+  result = num1 / num2
+  print(locals())
+
+# Write Checkpoint 3 here:
+def multiply(num1, num2):
+  product = num1 * num2
+  print(locals())
+
+
+
+print(' \n -- Local Namespace for divide -- \n')
+# Write Checkpoint 4 here:
+divide(3,4)
+
+print(' \n -- Local Namespace for multiply -- \n')
+# Write Checkpoint 5 here:
+multiply(4,50)
+
+print(' \n -- Local Namespace final -- \n')
+# Write Checkpoint 6 here:
+print(locals())
