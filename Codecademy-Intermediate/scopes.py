5856c146c1eeb25cb7431478086443c185a1b558
srac1777 <shashank.racherla@gmail.com>
2021-08-10 08:26:41 -0400

    scope2

--- Codecademy-Intermediate/scopes.py
+++ Codecademy-Intermediate/scopes.py
@@ -7,3 +7,17 @@ def painting(paint_colors, picture):
 
 
 painting(['Orange', 'White', 'Green'], 'Indian Flag')
+
+
+def calc_paint_amount(width, height):
+
+  square_feet = width * height
+  # Write your code below!
+
+  def calc_gallons():
+    return square_feet / 400
+  return calc_gallons()
+
+
+print('Number of paint gallons needed: ')
+print(str(calc_paint_amount(30, 20)))
