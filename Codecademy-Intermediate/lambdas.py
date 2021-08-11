5ec66aebe83c7e8445b6c7df8dab8189b0ef41f4
srac1777 <shashank.racherla@gmail.com>
2021-08-11 06:39:25 -0400

    lambda2

--- Codecademy-Intermediate/lambdas.py
+++ Codecademy-Intermediate/lambdas.py
@@ -5,3 +5,9 @@ add_three = lambda my_input: my_input + 3
 
 pop = add_three(2)
 print(pop)
+
+#if statement lambdas
+check_if_A_grade = lambda grade: 'Got an A!' if grade >= 90 else 'Did not get an A.'
+
+print(check_if_A_grade(90))
+
