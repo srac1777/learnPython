56732579f1a529c6069691bdd5cba471e48ab94e
srac1777 <shashank.racherla@gmail.com>
2021-08-25 22:24:10 -0400

    opoverloading

--- Codecademy-Intermediate/polymorphism.py
+++ Codecademy-Intermediate/polymorphism.py
@@ -29,3 +29,35 @@ meeting = [e, a, m]
 
 for user in meeting:
   user.say_id() # different objects having same behavior (no need to worry about which class it belongs to)
+
+
+# operator overloading
+class Employee():
+  new_id = 1
+
+  def __init__(self):
+    self.id = Employee.new_id
+    Employee.new_id += 1
+
+
+class Meeting:
+  def __init__(self):
+    self.attendees = []
+
+  def __add__(self, employee):
+    print("ID {} added.".format(employee.id))
+    self.attendees.append(employee)
+
+  # Write your code
+  def __len__(self): #overloading the len(obj) fn
+    return len(self.attendees)
+
+
+e1 = Employee()
+e2 = Employee()
+e3 = Employee()
+m1 = Meeting()
+m1 + e1
+m1 + e2
+m1 + e3
+print(len(m1))
