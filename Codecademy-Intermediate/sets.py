a131c5a2fe863d8e8039b395ac0645f44b7ab727
srac1777 <shashank.racherla@gmail.com>
2021-08-30 07:47:46 -0400

    sets3

--- Codecademy-Intermediate/sets.py
+++ Codecademy-Intermediate/sets.py
@@ -33,3 +33,30 @@ new_song_data = {}
 
 for key, val in song_data.items():
   new_song_data[key] = set(song_data[key]) | set(user_tag_data[key])
+  
+  song_data = {'Retro Words': ['pop', 'warm', 'happy', 'electronic', 'synth'],
+               'Wait For Limit': ['rap', 'upbeat', 'romance', 'relationship'],
+               'Stomping Cue': ['country', 'fiddle', 'party'],
+               'Lowkey Space': ['electronic', 'dance', 'synth', 'upbeat'],
+               'Back To Art': ['pop', 'sad', 'emotional', 'relationship'],
+               'Blinding Era': ['rap', 'intense', 'moving', 'fast'],
+               'Down To Green Hills': ['country', 'relaxing', 'vocal', 'emotional'],
+               'Double Lights': ['electronic', 'chill', 'relaxing', 'piano', 'synth']}
+
+user_liked_song = {'Back To Art': ['pop', 'sad', 'emotional', 'relationship']}
+user_disliked_song = {'Retro Words': [
+    'pop', 'warm', 'happy', 'electronic', 'synth']}
+
+# Checkpoint 1
+tag_diff = set(user_liked_song['Back To Art']) - \
+    set(user_disliked_song['Retro Words'])
+
+# Checkpoint 2
+recommended_songs = {}
+for key, val in song_data.items():
+    for tag in val:
+        if tag in tag_diff:
+            if key not in user_liked_song and key not in user_disliked_song:
+                recommended_songs[key] = val
+
+print(recommended_songs)
