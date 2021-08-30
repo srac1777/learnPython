3107eb2c338a27e863837fa27c041d7b23cb8902
srac1777 <shashank.racherla@gmail.com>
2021-08-30 07:37:03 -0400

    sets2

--- Codecademy-Intermediate/sets.py
+++ Codecademy-Intermediate/sets.py
@@ -16,3 +16,20 @@ for tag in bad_tags:
   tag_set.remove(tag)
 
 song_data_users['Retro Words'] = tag_set
+
+
+song_data = {'Retro Words': ['pop', 'warm', 'happy', 'electronic'],
+             'Wait For Limit': ['rap', 'upbeat', 'romance'],
+             'Stomping Cue': ['country', 'fiddle', 'party'],
+             'Lowkey Space': ['electronic', 'dance', 'synth']}
+
+user_tag_data = {'Lowkey Space': ['party', 'synth', 'fast', 'upbeat'],
+                 'Retro Words': ['happy', 'electronic', 'fun', 'exciting'],
+                 'Wait For Limit': ['romance', 'chill', 'rap', 'rhythmic'],
+                 'Stomping Cue': ['country', 'swing', 'party', 'instrumental']}
+
+# Write your code below!
+new_song_data = {}
+
+for key, val in song_data.items():
+  new_song_data[key] = set(song_data[key]) | set(user_tag_data[key])
